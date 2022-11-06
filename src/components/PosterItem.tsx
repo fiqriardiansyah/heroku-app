f6e71cde3ddfa66ab0b18bf067d29c878ee3bd7d
azariaberyl <beryl.naga@gmail.com>
2022-11-06 14:21:52 +0700

    feat: home UI As Hero

---
+++ src/components/PosterItem.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+
+interface Prop {
+    poster: import("models").Poster;
+}
+
+function PosterItem({ poster }: Prop) {
+    const {
+        title,
+        applicants,
+        date,
+        type_of_job: typeOfJob,
+        price,
+        description,
+        skills,
+        company,
+    } = poster;
+    if (typeOfJob === "task") {
+        return (
+            <div
+                style={{ borderBottom: "1px solid #7E7E7E" }}
+                id="poster-item"
+                className="flex flex-col justify-evenly my-3 h-68 pb-5 "
+            >
+                <h3 className="m-0">{title}</h3>
+                <div>
+                    <p className="m-0 text-sm">{`Owner: &ensp; ${applicants}`}</p>
+                    <p style={{ color: "#A7A7A7" }} className="m-0 text-10">
+                        {date}
+                    </p>
+                </div>
+                <p className="m-0 text-16">{` Fixed Price - ${price}`}</p>
+                <p style={{ color: "#828282" }} className="m-0 text-16">
+                    {description}
+                </p>
+                <div id="tag">
+                    {skills.map((skill) => (
+                        <button
+                            style={{ color: "#606060" }}
+                            className="px-5 py-2 rounded-2xl border-none cursor-pointer"
+                            type="button"
+                        >
+                            {skill}
+                        </button>
+                    ))}
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div
+            style={{ borderBottom: "1px solid #7E7E7E" }}
+            id="poster-item"
+            className="flex flex-col justify-evenly my-3 h-68 pb-5 "
+        >
+            <div>
+                <h3 className="m-0">{title}</h3>
+                <p className="m-0 text-sm">{company}</p>
+            </div>
+            <div>
+                <p className="m-0 text-sm">{`Owner: &ensp; ${applicants}`}</p>
+                <p style={{ color: "#A7A7A7" }} className="m-0 text-10">
+                    {date}
+                </p>
+            </div>
+            <p className="m-0 text-16">{` Fixed Price - ${price}`}</p>
+            <p style={{ color: "#828282" }} className="m-0 text-16">
+                {description}
+            </p>
+            <div id="tag">
+                {skills.map((skill) => (
+                    <button
+                        style={{ color: "#606060" }}
+                        className="px-5 py-2 rounded-2xl border-none cursor-pointer"
+                        type="button"
+                    >
+                        {skill}
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default PosterItem;
--- src/module/home/signin/as-hero.tsx
+++ src/module/home/signin/as-hero.tsx
@@ -1,15 +1,116 @@
+import { Button, Input, Space } from "antd";
 import { StateContext } from "context/state";
-import { Service } from "models";
-import React, { useContext } from "react";
+import { getDatabase } from "firebase/database";
+import { Poster, Service } from "models";
+import React, { useContext, useEffect, useState } from "react";
 import { useMutation, useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import authService from "services/auth";
 import heroService from "services/hero";
+import RealtimeDatabase from "services/utils/realtime-database";
 import { SERVICE_HERO_PATH } from "utils/routes";
+import NoJobs from "assets/svgs/no-jobs.svg";
+import PosterItem from "components/PosterItem";
 
 function HomeAsHero() {
     const user = authService.CurrentUser();
-    return <div className="wfull">as hero</div>;
+    const [posters, setPosters] = useState<Poster[]>([]);
+    const [filter, setfilter] = useState("");
+    const db = new RealtimeDatabase(getDatabase());
+    useEffect(() => {
+        const getPosters = async () => {
+            const allPosters = await db.GetAllPoster();
+            setPosters(allPosters);
+        };
+        getPosters();
+    }, []);
+    if (posters.length === 0) {
+        return (
+            <div className="w-full h-full">
+                <div
+                    className="w-full py-2.5 px-5 bg-white rounded-xl mt-7 border border-solid"
+                    style={{ borderColor: "#B4B4B4" }}
+                >
+                    <h2
+                        style={{ fontFamily: "Montserrat, sans-serif" }}
+                        className="text-2.5xl font-semibold"
+                    >
+                        Explore Jobs
+                    </h2>
+                    <div className="flex gap-x-2.5">
+                        <Input
+                            style={{ borderRadius: "4px" }}
+                            placeholder="Search keyword title like “Build landing page using React “"
+                            value={filter}
+                            onInput={(e) => {
+                                setfilter(e.currentTarget.value);
+                            }}
+                        />
+                        <Button
+                            style={{
+                                borderRadius: "5px",
+                            }}
+                            size="large"
+                            type="primary"
+                        >
+                            Search
+                        </Button>
+                    </div>
+                </div>
+                <div
+                    style={{ border: "1px solid #CECECE" }}
+                    className="w-full mt-5 py-3 px-5 bg-white rounded-xl h-80vh flex justify-center align-middle"
+                >
+                    <img src={NoJobs} alt="No Jobs" className="w-96 " />
+                </div>
+            </div>
+        );
+    }
+
+    const filteredPosters = posters.filter((poster) => poster.title === filter);
+    return (
+        // Search
+        <div className="w-full h-full">
+            <div
+                className="w-full py-2.5 px-5 bg-white rounded-xl mt-7 border border-solid"
+                style={{ borderColor: "#B4B4B4" }}
+            >
+                <h2
+                    style={{ fontFamily: "Montserrat, sans-serif" }}
+                    className="text-2.5xl font-semibold"
+                >
+                    Explore Jobs
+                </h2>
+                <div className="flex gap-x-2.5">
+                    <Input
+                        style={{ borderRadius: "4px" }}
+                        placeholder="Search keyword title like “Build landing page using React “"
+                        value={filter}
+                        onInput={(e) => setfilter(e.currentTarget.value)}
+                    />
+                    <Button
+                        style={{
+                            borderRadius: "5px",
+                        }}
+                        size="large"
+                        type="primary"
+                    >
+                        Search
+                    </Button>
+                </div>
+            </div>
+
+            {/* List item */}
+            <div
+                style={{ border: "1px solid #CECECE" }}
+                className="mt-5 py-3 px-5 bg-white rounded-xl h-full"
+            >
+                {filteredPosters.map((poster) => {
+                    return <PosterItem poster={poster} />;
+                })}
+            </div>
+        </div>
+    );
 }
 
 export default HomeAsHero;
