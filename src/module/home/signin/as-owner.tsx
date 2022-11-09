b3c57e59dde325d785ea1b02895321155890c01f
Abdul Wasi <abdul.wasi.al.afif@gmail.com>
2022-11-10 01:38:14 +0700

    feat: add UI MyPost, HomeAsOwner and fixed code

--- src/module/home/signin/as-owner.tsx
+++ src/module/home/signin/as-owner.tsx
@@ -1,16 +1,117 @@
 import { StateContext } from "context/state";
 import { Poster } from "models";
 import moment from "moment";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { Card, AutoComplete } from "antd";
 import { useMutation, useQuery } from "react-query";
 import authService from "services/auth";
 import ownerService from "services/owner";
 
+const mockVal = (str: string, repeat = 1) => ({
+    value: str.repeat(repeat),
+});
+
 function HomeAsOwner() {
     const user = authService.CurrentUser();
     const { state } = useContext(StateContext);
+    const [value, setValue] = useState("");
+    const [options, setOptions] = useState<{ value: string }[]>([]);
 
-    return <div className="wfull">as owner</div>;
+    const onSearch = (searchText: string) => {
+        setOptions(
+            !searchText
+                ? []
+                : [
+                      mockVal(searchText),
+                      mockVal(searchText, 2),
+                      mockVal(searchText, 3),
+                  ]
+        );
+    };
+
+    const onSelect = (data: string) => {
+        console.log("onSelect", data);
+    };
+
+    const onChange = (data: string) => {
+        setValue(data);
+    };
+
+    return (
+        <div className="grid">
+            <br />
+            <Card title="Explore Services">
+                <AutoComplete
+                    options={options}
+                    style={{ width: "100%" }}
+                    onSelect={onSelect}
+                    onSearch={onSearch}
+                    placeholder="Search Service"
+                />
+            </Card>
+            <br />
+            <Card className="CONTAINER">
+                {/* UNTUK LOOP ITEM */}
+                {/* {notes.length > 0 ? ( */}
+                <div className="grid grid-cols-1 md:grid-cols-4">
+                    {/* {ownerService.database.map((ownerService) => { */}
+                    {/* return ( */}
+                    <div className="mb-4 flex flex-col justify-center pb-10 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                        <div className=" m-4 flex flex-row">
+                            <img
+                                className="w-12 h-12 rounded-full shadow-lg"
+                                src="user?.photoURL"
+                                alt=""
+                            />
+                            <div className="ml-3">
+                                <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">
+                                    {user?.displayName}
+                                </h5>
+                                <span className="text-sm text-gray-500 dark:text-gray-400">
+                                    {/* {user?.workField}  */}
+                                    Visual Designer
+                                </span>
+                            </div>
+                        </div>
+                        <div className=" m-4 flex flex-col">
+                            <img
+                                className="rounded-lg"
+                                // src="{ownerService.photoService}"
+                                src="https://www.wallpapers13.com/wp-content/uploads/2016/01/Sunset-desktop-background-498686-840x525.jpg"
+                                alt="Service"
+                            />
+                            <br />
+                            <p className="text-gray-900 dark:text-white text-sm font-bold">
+                                {/* {ownerService.titleService} */}
+                                Some quick example text to build on the card
+                                title and make up the bulk of the cards content.
+                            </p>
+
+                            <p className="text-gray-400 dark:text-white mb-4">
+                                {/* {ownerService.detailService} */}
+                                Some quick example text to build on the card
+                                title and make up the bulk of the cards content.
+                            </p>
+                            <p className="inline-flex py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">
+                                {/* {ownerService.priceService} */}
+                                Rp. 210.100,00
+                            </p>
+                        </div>
+                    </div>
+                    {/* HAPUS DIV 1 BARIS DI ATAS INI DAN JANGAN HAPUS DIV 1 BARIS  DI DIBAWAH INI */}
+                    {/* </div>);})} */}
+                </div>
+                {/* ) : ( */}
+                <div className="flex flex-auto">
+                    <p className="text-gray-400 dark:text-white">
+                        No found services
+                    </p>
+                </div>
+                {/* )} */}
+            </Card>
+        </div>
+        // </Layout>
+    );
 }
 
 export default HomeAsOwner;
--- src/pages/as-owner/my-post.tsx
+++ src/pages/as-owner/my-post.tsx
@@ -1,8 +1,107 @@
 import Layout from "components/common/layout";
-import React from "react";
+import { AutoComplete, Card, Tabs } from "antd";
+import React, { useState } from "react";
+import TableApp from "components/common/table";
+
+const Swal = require("sweetalert2");
+
+const mockVal = (str: string, repeat = 1) => ({
+    value: str.repeat(repeat),
+});
+
+const items = [
+    { label: "Task", key: "task-tabs", children: <TableApp /> }, // remember to pass the key prop
+    { label: "Hiring", key: "hiring-tabs", children: <TableApp /> },
+];
 
 function MyPost() {
-    return <Layout>My post</Layout>;
+    const [value, setValue] = useState("");
+    const [options, setOptions] = useState<{ value: string }[]>([]);
+
+    const onSearch = (searchText: string) => {
+        setOptions(
+            !searchText
+                ? []
+                : [
+                      mockVal(searchText),
+                      mockVal(searchText, 2),
+                      mockVal(searchText, 3),
+                  ]
+        );
+    };
+
+    const onSelect = (data: string) => {
+        console.log("onSelect", data);
+    };
+
+    const onChange = (data: string) => {
+        setValue(data);
+    };
+
+    const myPostOnClick = () => {
+        Swal.fire(
+            "Apa itu My Post?",
+            "That thing is still around?",
+            "question"
+        );
+    };
+
+    return (
+        <Layout>
+            <br />
+            <div className="flex flex-row space-x-2">
+                <div className="flex flex-0 flex-row md:flex-1">
+                    <h3>My Post </h3>
+                    <div>
+                        <button
+                            type="button"
+                            className="rounded-full bg-gray border-0"
+                            onClick={myPostOnClick}
+                        >
+                            ?
+                        </button>
+                    </div>
+                </div>
+                <div className="flex flex-1 flex-row">
+                    <AutoComplete
+                        options={options}
+                        style={{ width: "100%" }}
+                        onSelect={onSelect}
+                        onSearch={onSearch}
+                        placeholder="Search Service"
+                    />
+                    <br />
+                    <button
+                        type="button"
+                        className="bg-primary border-0 text-white w-full md:w-1/3"
+                        onClick={myPostOnClick}
+                    >
+                        Create new post
+                    </button>
+                </div>
+            </div>
+            <br />
+            <Card>
+                <Tabs items={items} />
+                {/* <Tabs defaultActiveKey="1">
+                    <Panes tab="Task" key="task-tabs">
+                        <TableApp />
+                    </Panes>
+                    <Panes tab="Hiring" key="hiring-tabs">
+                        <TableApp />
+                    </Panes>
+                </Tabs> */}
+                {/* <Tabs items={items}> */}
+                {/* <Tabs.TabPane tab="Task" key="task-tabs">
+                    <TableApp />
+                </Tabs.TabPane>
+                <Tabs.TabPane tab="Hiring" key="hiring-tabs">
+                    Conten
+                </Tabs.TabPane> */}
+                {/* </Tabs> */}
+            </Card>
+        </Layout>
+    );
 }
 
 export default MyPost;
