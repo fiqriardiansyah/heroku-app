7a40b16192414d463f3245f30a3de2355cd8abf8
azariaberyl <beryl.naga@gmail.com>
2022-11-15 07:45:15 +0700

    feat(my-service.tsx): update ui and add functionality

--- src/pages/as-hero/my-service.tsx
+++ src/pages/as-hero/my-service.tsx
@@ -1,11 +1,17 @@
 /* eslint-disable no-prototype-builtins */
 import { AutoComplete, Button, Card, Table, Tabs } from "antd";
 import Layout from "components/common/layout";
-import { UserContext } from "context/user";
+import { getDatabase } from "firebase/database";
 import React, { useContext, useState } from "react";
+import authService from "services/auth";
+import HeroServiceSupport from "services/support/hero";
 import Swal from "sweetalert2";
+import NoJobs from "assets/svgs/no-jobs.svg";
+import { useNavigate } from "react-router-dom";
+import { CREATE_SERVICE_PATH, SERVICE_HERO_PATH } from "utils/routes";
 
 interface ServiceActiveDataSource {
+    id: any;
     key: any;
     no: number;
     title: string;
@@ -16,6 +22,7 @@ interface ServiceActiveDataSource {
 }
 
 interface ServiceDraftDataSource {
+    id: any;
     key: any;
     no: number;
     created: string;
@@ -23,9 +30,12 @@ interface ServiceDraftDataSource {
 }
 
 interface ServiceListProps {
-    activeData: ServiceActiveDataSource[];
-    draftData: ServiceDraftDataSource[];
+    activeData?: ServiceActiveDataSource[];
+    draftData?: ServiceDraftDataSource[];
     tab: "active" | "draft";
+    onDraftDelete: any;
+    onActiveServiceClick: any;
+    onDraftServiceClick: any;
 }
 
 const mockVal = (data: string[], query: string) => {
@@ -34,85 +44,143 @@ const mockVal = (data: string[], query: string) => {
     return result;
 };
 
-function ServicesList({ activeData = [], draftData = [], tab }: ServiceListProps) {
-    let columns;
+function ServicesList({ activeData = [], draftData = [], tab, onDraftDelete, onActiveServiceClick, onDraftServiceClick }: ServiceListProps) {
     if (tab === "active") {
-        columns = [
-            {
-                title: "No",
-                dataIndex: "no",
-                key: "no",
-            },
-            {
-                title: "Service",
-                dataIndex: "title",
-                key: "title",
-            },
-            {
-                title: "Finish",
-                dataIndex: "finish",
-                key: "finish",
-            },
-            {
-                title: "Order",
-                dataIndex: "order",
-                key: "order",
-            },
-            {
-                title: "View",
-                dataIndex: "view",
-                key: "view",
-            },
-            {
-                title: "Request",
-                dataIndex: "request",
-                key: "request",
-            },
-        ];
+        if (activeData.length > 0) {
+            return (
+                <Card>
+                    <table className="w-full">
+                        <thead style={{ color: "#6A6A6A", borderBottom: "1px solid #D7D7D7", fontSize: "16px" }} className="w-full ">
+                            <tr className="w-full flex gap-4">
+                                <th className="p-1 basis-9">No</th>
+                                <th className="grow p-1 text-left">Service</th>
+                                <th className="p-1 basis-1/12 text-left">Finish</th>
+                                <th className="p-1 basis-1/12 text-left">Order</th>
+                                <th className="p-1 basis-1/12 text-left">View</th>
+                                <th className="p-1 basis-1/12 text-left">Request</th>
+                            </tr>
+                        </thead>
+                        <tbody className="w-full">
+                            {activeData.map((data) => (
+                                <tr
+                                    onClick={() => onActiveServiceClick(data.id)}
+                                    key={data.id}
+                                    className="w-full flex gap-4 py-2 hover:bg-gray-50 hover:cursor-pointer"
+                                >
+                                    <td className="p-1 basis-9">{data.no}</td>
+                                    <td className="grow p-1 text-left">{data.title}</td>
+                                    <td className="p-1 basis-1/12">{data.finish}</td>
+                                    <td className="p-1 basis-1/12">{data.order}</td>
+                                    <td className="p-1 basis-1/12">{data.view}</td>
+                                    <td className="p-1 basis-1/12">{data.request}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </Card>
+            );
+        }
+        return (
+            <div
+                style={{ border: "1px solid #CECECE" }}
+                className="w-full mt-5 py-3 px-5 bg-white rounded-xl h-80vh flex justify-center align-middle"
+            >
+                <img src={NoJobs} alt="No Jobs" className="w-96 " />
+            </div>
+        );
+    }
 
+    if (draftData.length > 0) {
         return (
             <Card>
-                <Table dataSource={activeData} pagination={{ position: [] }} columns={columns} />
+                <table className="w-full">
+                    <thead style={{ color: "#6A6A6A", borderBottom: "1px solid #D7D7D7", fontSize: "16px" }} className="w-full ">
+                        <tr className="w-full flex gap-4">
+                            <th className="p-1 basis-9">No</th>
+                            <th className="grow p-1 text-left">Service</th>
+                            <th className="p-1 basis-3/12 text-left">Created at</th>
+                            <th className="p-1 basis-3/12 text-left">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody className="w-full">
+                        {draftData.map((data) => (
+                            <tr onClick={onDraftServiceClick} key={data.id} className="w-full flex gap-4 py-2 hover:bg-gray-50 hover:cursor-pointer">
+                                <td className="p-1 basis-9">{data.no}</td>
+                                <td className="grow p-1 text-left">{data.title}</td>
+                                <td className="p-1 basis-3/12 text-left">{data.created}</td>
+                                <td className="p-1 basis-3/12 text-left">
+                                    <Button onClick={() => onDraftDelete(data.id)} type="primary" danger>
+                                        Delete
+                                    </Button>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </Card>
         );
     }
 
-    columns = [
-        {
-            title: "No",
-            dataIndex: "no",
-            key: "no",
-        },
-        {
-            title: "Service",
-            dataIndex: "title",
-            key: "title",
-        },
-        {
-            title: "Created",
-            dataIndex: "created",
-            key: "created",
-        },
-        {
-            title: "Action",
-            dataIndex: "action",
-            key: "action",
-        },
-    ];
-
     return (
-        <Card>
-            <Table dataSource={draftData} pagination={{ position: [] }} columns={columns} />
-        </Card>
+        <div style={{ border: "1px solid #CECECE" }} className="w-full mt-5 py-3 px-5 bg-white rounded-xl h-80vh flex justify-center align-middle">
+            <img src={NoJobs} alt="No Jobs" className="w-96 " />
+        </div>
     );
 }
 
 function MyService() {
+    const db = new HeroServiceSupport(getDatabase());
+    const user = authService.CurrentUser();
+    const navigate = useNavigate();
+
     const [value, setValue] = useState("");
     const [tab, setTab] = useState<"active" | "draft">("active");
     const [options, setOptions] = useState<{ value: string }[]>([]);
+    const [draftDataSource, setDraftDataSource] = useState<ServiceDraftDataSource[]>([]);
 
     const inActiveButtonStyle = { border: "none", backgroundColor: "inherit" };
+
+    const onDraftDelete = (id: any) => {
+        const newDraft = draftDataSource.filter((data) => data.id !== id);
+        setDraftDataSource(newDraft);
+    };
+
+    const onActiveServiceClick = (id: any) => {
+        navigate(`${SERVICE_HERO_PATH}/${user?.uid}/${id}`);
+    };
+
+    const onDraftServiceClick = () => {
+        navigate(`${CREATE_SERVICE_PATH}`);
+    };
+
+    const onCreateServiceBtnClick = () => {
+        navigate(`${CREATE_SERVICE_PATH}`);
+    };
+
+    const onDraftBtnClick = () => {
+        setTab("draft");
+    };
+
+    const onActiveBtnClick = () => {
+        setTab("active");
+    };
+
+    React.useEffect(() => {
+        async function getServices() {
+            // const testService = await db.GetAllMyServices({ user?.uid });
+            // console.log(testService);
+            setDraftDataSource([
+                {
+                    id: 3,
+                    created: "12 February 2022",
+                    key: "1",
+                    no: 1,
+                    title: "Saya menggambar",
+                },
+            ]);
+        }
+        getServices();
+    }, []);
 
     const onSelect = (data: string) => {
         console.log("onSelect", data);
@@ -126,16 +194,9 @@ function MyService() {
         Swal.fire("Apa itu My Services?", "That thing is still around?", "question");
     };
 
-    const onDraftBtnClick = () => {
-        setTab("draft");
-    };
-
-    const onActiveBtnClick = () => {
-        setTab("active");
-    };
-
     const activeDataSource: ServiceActiveDataSource[] = [
         {
+            id: 1,
             key: "1",
             no: 1,
             title: "Saya bisa membuat animasi 3d menggunakan blender, adobe family dan lainnya",
@@ -145,6 +206,7 @@ function MyService() {
             view: 1,
         },
         {
+            id: 2,
             key: "2",
             no: 2,
             title: "Saya bisa mengedit fotomu disamping pohon",
@@ -152,15 +214,6 @@ function MyService() {
             order: 2,
             request: 2,
             view: 2,
-        },
-    ];
-
-    const draftDataSource: ServiceDraftDataSource[] = [
-        {
-            created: "12 February 2022",
-            key: "1",
-            no: 1,
-            title: "Saya menggambar",
         },
     ];
 
@@ -207,9 +260,9 @@ function MyService() {
                         placeholder="Search Service"
                     />
                     <br />
-                    <button type="button" className="bg-primary border-0 text-white md:w-1/3 w-fit" onClick={myServicesOnClick}>
+                    <Button type="primary" className=" md:w-1/3 w-fit" onClick={onCreateServiceBtnClick}>
                         Create New Service
-                    </button>
+                    </Button>
                 </div>
             </div>
             <br />
@@ -230,7 +283,14 @@ function MyService() {
                 )}
             </div>
 
-            <ServicesList tab={tab} draftData={draftDataSource} activeData={activeDataSource} />
+            <ServicesList
+                tab={tab}
+                activeData={activeDataSource}
+                draftData={draftDataSource}
+                onDraftDelete={onDraftDelete}
+                onActiveServiceClick={onActiveServiceClick}
+                onDraftServiceClick={onDraftServiceClick}
+            />
         </Layout>
     );
 }
