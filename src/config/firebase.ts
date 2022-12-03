6806e6b6da424b4c3e0aa0319f6acd6fe2464485
fiqriardiansyah <fiqriardian92@gmail.com>
2022-12-03 22:09:06 +0700

    ci: firebase rules

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -7,7 +7,9 @@ class ConfigFirebase {
         projectId: string;
         storageBucket: string;
         messagingSenderId: string;
+        databaseURL: string;
         appId: string;
+        measurementId: string;
     };
 
     app: FirebaseApp;
@@ -17,9 +19,11 @@ class ConfigFirebase {
             apiKey: process.env.REACT_APP_API_KEY as string,
             authDomain: process.env.REACT_APP_AUTH_DOMAIN as string,
             projectId: process.env.REACT_APP_PROJECT_ID as string,
+            databaseURL: process.env.REACT_APP_DATABASE_URL as string,
             storageBucket: process.env.REACT_APP_STORAGE_BUCKET as string,
             messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID as string,
             appId: process.env.REACT_APP_APP_ID as string,
+            measurementId: process.env.REACT_APP_MEASUREMENT_ID as string,
         };
         this.app = initializeApp(this.firebaseConfig);
     }
